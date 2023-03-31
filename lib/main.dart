a1ec08a7c6faf479c33a2e7fe440e343aad50bdb
Supada Sutta <kreamkream33@gmail.com>
2023-03-31 16:09:37 +0700

    edit page

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,6 @@ import 'package:reun_kra_jok/screen/login.dart';
 import 'package:reun_kra_jok/screen/payment.dart';
 import 'package:reun_kra_jok/screen/start_app.dart';
 
-void main() {  
-        //  runApp(const StartPage());
-        runApp(SignUpPage());
-        // runApp(DescPage());
-        // runApp(const HomePage());
-        // runApp(Cart());
-        // runApp(PaymentPage());
-        // runApp(CompletePage());
-        //  runApp(LoginPage());
-}  
-
+void main() {
+  runApp(StartPage());
+}
