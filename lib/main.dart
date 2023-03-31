f8c3247f8762cd50d0ac9e9758b890e1a2d7e1b2
Supada Sutta <kreamkream33@gmail.com>
2023-03-31 16:13:35 +0700

    edit page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:reun_kra_jok/screen/addSlip.dart';
-import 'package:reun_kra_jok/screen/cart.dart';
-import 'package:reun_kra_jok/screen/complete.dart';
-import 'package:reun_kra_jok/screen/decsrip_plant.dart';
-import 'package:reun_kra_jok/screen/SignUpPage.dart';
-import 'package:reun_kra_jok/screen/home.dart';
 import 'package:reun_kra_jok/screen/login.dart';
-import 'package:reun_kra_jok/screen/payment.dart';
-import 'package:reun_kra_jok/screen/start_app.dart';
 
 void main() {
-  runApp(StartPage());
+  runApp(LoginPage());
 }
